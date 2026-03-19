6987a6e555d52125bdbadf73d00db8a43af43265
Jiucheng-night <jiuchengnight@gmail.com>
2026-03-19 11:17:11 +0800

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,27 @@
+# models/__init__.py
+
 from .cnn import SimpleCNN
 from .resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
+from .vgg import VGG
+from .mobilenet import MobileNet
 
-def get_model(name='cnn'):
+def get_model(name):
+    """
+    返回模型实例
+    name: str, 模型名称，可选：
+        'cnn'       -> SimpleCNN
+        'resnet18'  -> ResNet18
+        'resnet34'  -> ResNet34
+        'resnet50'  -> ResNet50
+        'resnet101' -> ResNet101
+        'resnet152' -> ResNet152
+        'vgg11'     -> VGG11
+        'vgg13'     -> VGG13
+        'vgg16'     -> VGG16
+        'vgg19'     -> VGG19
+    """
     name = name.lower()
     if name == 'cnn':
-        from .cnn import SimpleCNN
         return SimpleCNN()
     elif name == 'resnet18':
         return ResNet18()
@@ -16,5 +33,10 @@ def get_model(name='cnn'):
         return ResNet101()
     elif name == 'resnet152':
         return ResNet152()
+    elif name.startswith('vgg'):
+        # 例如 vgg11, vgg16
+        return VGG(name.upper())
+    elif name == "mobilenet":
+        return MobileNet()
     else:
-        raise ValueError(f'Unknown model name {name}')
+        raise ValueError(f"Unknown model name: {name}")
